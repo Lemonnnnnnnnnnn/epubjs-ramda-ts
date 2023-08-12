08a754593b81d2a036d7ab6e0153e06d3bbd9917
soul <lemon.asd99@gmail.com>
2023-08-13 00:04:29 +0800

    feat: edit publish config

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 ```js
 import epub from '@lemon/epub-parser'
 
+// the relative path with current workspace root path
+const TEST_FILE = path.resolve("src/public/1.epub")
+
 const fn = async () => {
-    const book = await epub("1.epub");
+    const book = await epub(TEST_FILE);
     const toc = book.getTOC()
     console.log(toc);
 }
--- src/test.ts
+++ src/test.ts
@@ -1,15 +1,8 @@
 import path from 'path'
 import epub from './main';
 
-const TEST_FILE = path.join(
-  process.cwd(),
-  "./src/public/1.epub",
-);
-const TEST_FILE2 = path.join(
-  process.cwd(),
-  "./src/public/2.epub",
-);
-
+const TEST_FILE = path.resolve("src/public/1.epub")
+const TEST_FILE2 = path.resolve("src/public/2.epub")
 
 const test = async () => {
     const book = await epub(TEST_FILE);
